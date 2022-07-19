7327bd11c948e09fc63b145f6f555754413bf53f
Rostislav Skripnik <rostislav.skripnik@dataart.com>
2022-07-19 19:27:28 +0300

    Connect Mongo Db to Shopping.API

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Shopping.API.Data;
 using Shopping.API.Models;
 
@@ -12,17 +14,20 @@ namespace Shopping.API.Controllers
     [Route("[controller]")]
     public class ProductController
     {
+        private readonly ProductContext _context;
         private readonly ILogger<ProductController> _logger;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<Product> Get()
+        public async Task<IEnumerable<Product>> Get()
         {
-            return ProductContext.Products;
+            var t = await _context.Products.Find(p => true).ToListAsync();
+            return t;
         }
     }
 }
